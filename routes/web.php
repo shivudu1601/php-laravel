34aac48b7397cb2af5384d780d811bed265d0ba7
shivudu1601 <shivanigorantala@gmail.com>
2024-12-03 22:09:04 +0530

    php laravel layouts

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ Route::get('/mul/{num1}/{num2}', [CalculatorController::class, 'mul']);
 Route::get('/div/{num1}/{num2}', [CalculatorController::class, 'div']);
 Route::get('/login', [UserController::class, 'login']);
 Route::get('/register', [UserController::class, 'register']);
+
+// Login and registration routes 
+
+ Route::get('authlogin', [App\Http\Controllers\AuthManager::class, 'login'])->name('authlogin'); 
+Route::get('authregister', [App\Http\Controllers\AuthManager::class, 'register'])->name('authregister');
